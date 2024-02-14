e76285e41df52a12548ae50182a6153ca440ca4e
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-14 17:08:06 +0100

    Complete exercise

--- resources/views/single_film.blade.php
+++ resources/views/single_film.blade.php
@@ -1,4 +1,18 @@
 @extends('layout.app')
 @section('content')
-    {{ $movie['title'] }}
+    <div class="container my-5">
+        <a href="{{ route('films') }}" class="btn btn-sm btn-primary">Torna indietro</a>
+        <div class="row mt-5">
+            <div class="col-12 d-flex justify-content-center">
+                <div class="card p-3">
+                    <h4 class="card-title text-center"> {{ $movie['title'] }} </h4>
+                    <p class="card-text text-center">{{ $movie['original_title'] }}</p>
+                    <span class="card-text">Nazione: {{ $movie['nationality'] }}</span>
+                    <span class="card-text">Data di uscita: {{ $movie['date'] }}</span>
+                    <span class="card-text">Voto: {{ $movie['vote'] }}</span>
+                    <span class="card-text">Descrizione: </span>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
