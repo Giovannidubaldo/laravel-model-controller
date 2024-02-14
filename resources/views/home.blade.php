b992a8e82c2b36b5f55bb431406fa130045d9b8f
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-14 16:28:37 +0100

    Created file app in layout's folder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
+@extends('layout.app')
+@section('content')
     <div class="container my-5">
         <div class="row">
             <h1 class="text-center">Film List</h1>
@@ -34,7 +18,4 @@
             @endforeach
         </div>
     </div>
-
-</body>
-
-</html>
+@endsection
