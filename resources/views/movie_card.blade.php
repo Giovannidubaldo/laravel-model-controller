6543339af5cb216d1fca59e4f6d4f471721867bf
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-14 17:07:50 +0100

    Added superbonus

--- resources/views/movie_card.blade.php
+++ resources/views/movie_card.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.app')
 @section('content')
     <div class="container my-5">
+        <a href="{{ route('home') }}" class="btn btn-sm btn-primary">Torna indietro</a>
         <div class="row">
             <h1 class="text-center">Film List</h1>
         </div>
@@ -8,7 +9,8 @@
             @foreach ($movies as $movie)
                 <div class="col-6 mt-5">
                     <div class="card p-2">
-                        <h4 class="card-title text-center"><a href="">{{ $movie['title'] }}</a></h4>
+                        <h4 class="card-title text-center"><a
+                                href="{{ route('singleFilm', ['films' => $movie->id]) }}">{{ $movie['title'] }}</a></h4>
                         <p class="card-text text-center">{{ $movie['original_title'] }}</p>
                         <span class="card-text">Nazione: {{ $movie['nationality'] }}</span>
                         <span class="card-text">Data di uscita: {{ $movie['date'] }}</span>
