173ab828dd2d7350c6e86f9a71bd97b97e2c4ba0
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-14 16:25:37 +0100

    Created card with Movies info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,24 @@
 </head>
 
 <body>
-
+    <div class="container my-5">
+        <div class="row">
+            <h1 class="text-center">Film List</h1>
+        </div>
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-6 mt-5">
+                    <div class="card p-2">
+                        <h4 class="card-title text-center">{{ $movie['title'] }}</h4>
+                        <p class="card-text text-center">{{ $movie['original_title'] }}</p>
+                        <span class="card-text">Nazione: {{ $movie['nationality'] }}</span>
+                        <span class="card-text">Data di uscita: {{ $movie['date'] }}</span>
+                        <span class="card-text">Voto: {{ $movie['vote'] }}</span>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
 </body>
 
